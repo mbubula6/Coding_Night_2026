ed55e6fe0ac5f6642f48a39b19b94e05839b889f
mbubula6 <mbubula6@gmail.com>
2026-03-21 04:45:11 +0100

    segmented data. WE CAN FINALLY GO TO MODELS

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from supabase import create_client
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 url = "https://euinlbrrdvzqeaniakyo.supabase.co"
 key = ""
 supabase = create_client(url, key)
+
+# GET DATA FUNCTIONS
 
 def get_user_data(user_uuid):
     """
@@ -19,8 +22,77 @@ def get_user_data(user_uuid):
         print(f"Error: {e}")
         return None
 
+def get_user_preferences(user_uuid):
+    """
+    Fetches preferences for a specific ID.
+    :param user_uuid:
+    :return preferences:
+    """
+    try:
+        response = supabase.table("user_preferences").select("*").eq("id_user", user_uuid).execute()
+        return response.data
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def prepare_dataframe(raw_data, preferences):
+    flat_list = []
+
+    for order in raw_data:
+        user = order.get('user_data', {})
+
+        row = {
+            'dish_id': order.get('id_dish'),
+            'created_at': order.get('created_at'),
+            'temp': order.get('temp_celsius'),
+            'weather': order.get('weather'),
+            'user_gender': user.get('gender'),
+            'user_class': user.get('class'),
+            'is_student': user.get('is_student'),
+            'is_vegetarian': preferences[0]['is_vegetarian'],
+            'is_vegan': preferences[0]['is_vegan'],
+            'is_nut_allergy': preferences[0]['is_nut_allergy'],
+            'is_gluten_allergy': preferences[0]['is_gluten_allergy'],
+            'is_shellfish_allergy': preferences[0]['is_shellfish_allergy'],
+            'is_dairy_allergy':preferences[0]['is_dairy_allergy'],
+            'spice_tolerance': preferences[0]['spice_tolerance'],
+            'sweetness_preference': preferences[0]['sweetness_preference'],
+            'warm': preferences[0]['warm'],
+            'cold': preferences[0]['cold'],
+            'with_drink': preferences[0]['with_drink'],
+            'preferred_cuisine': preferences[0]['preferred_cuisine']
+        }
+        flat_list.append(row)
+
+    dataframe = pd.DataFrame(flat_list)
+
+    # THE FIX:
+    dataframe['created_at'] = pd.to_datetime(dataframe['created_at'], format='ISO8601')
+
+    # ADD THESE for your model:
+    dataframe['hour'] = dataframe['created_at'].dt.hour
+    dataframe['day_of_week'] = dataframe['created_at'].dt.dayofweek
+
+    return dataframe
+
+# GETTING DATA
 
 target_user_id = "03ec4093-a950-48ea-a95e-7459b945aeda"
 user_info = get_user_data(target_user_id)
-print(user_info)
+# print(user_info)
+
+preferences = get_user_preferences(target_user_id)
+# print(preferences)
+df = prepare_dataframe(user_info,preferences)
+print(df)
+
+# SEGMENTING DATA (BAGGING)
+
+x = df.drop('dish_id', axis=1)
+y = df['dish_id']
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42
+)
 
